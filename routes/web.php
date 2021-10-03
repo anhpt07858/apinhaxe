94f575ca89b5110372265abb36fa11110bad8423
anhpt <anhpt07858@gmail.com>
2021-10-03 23:20:53 +0700

    make it better

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any?}')->where('any','^(?!api).*$');
+Route::get('/{any?}',function(){
+    return view('welcome');
+})->where('any','^(?!api).*$');
 
