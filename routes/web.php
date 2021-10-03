1189cf824c4bf91d20ca8fd487a7faf7b19afe15
anhpt <anhpt07858@gmail.com>
2021-10-03 23:11:42 +0700

    make it better

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/{any?}')->where('any','^(?!api).*$');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
